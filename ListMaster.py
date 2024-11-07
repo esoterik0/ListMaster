5104417eb1a662a7b964b21cfadce3fec85b5a54
mark <mminshall@topcon.com>
2024-11-06 17:27:36 -0800

    private members

--- ListMaster.py
+++ ListMaster.py
@@ -23,7 +23,7 @@ HEIGHT = 25
 SINGLE = 5
 
 
-class ListMaster:  # pylint: disable=too-many-instance-attributes
+class ListMaster:  # pylint: disable=too-many-instance-attributes, too-few-public-methods
     """
     Tkinter UI for generating random choices, and creating tables and formulas.
     """
@@ -37,16 +37,16 @@ class ListMaster:  # pylint: disable=too-many-instance-attributes
         self.result_list = []
         self.num = SINGLE
 
-        self.gen_main()
-        self.gen_what()
-        self.gen_page()
-        self.gen_results()
-        self.gen_buttons()
-        self.gen_form_buttons()
-        self.gen_clipboard()
-        self.ungrid_form()
-        self.ungrid_roll()
-        self.config_grid()
+        self._gen_main()
+        self._gen_what()
+        self._gen_page()
+        self._gen_results()
+        self._gen_buttons()
+        self._gen_form_buttons()
+        self._gen_clipboard()
+        self._ungrid_form()
+        self._ungrid_roll()
+        self._config_grid()
 
         if autostart:
             self.start()
@@ -55,25 +55,25 @@ class ListMaster:  # pylint: disable=too-many-instance-attributes
         "Runs the tk main loop"
         self.root.mainloop()
 
-    def gen_main(self):
+    def _gen_main(self):
         "Generates the main frame that all widgets will be part of"
         self.main = ttk.Frame(self.root, padding=5, width=WIDTH, height=HEIGHT)
         self.main.grid(column=0, row=0, sticky=(N, S, E, W))
 
-    def gen_what(self):
+    def _gen_what(self):
         "Generates the what frame, for choosing what category"
         self.what_frame = ridge_frame(self.main)
         self.what_frame.grid(column=0, row=0, sticky=(N, S, E, W))
 
         self.what_list = [dat.All_tables, dat.Maze_Rats_pages, dat.formulas]
-        what_choices = ["All tables", "Maze Rats pages", "Formulas"]
-        what_choice_var = tk.StringVar(value=what_choices)
+        self.what_choices = ["All tables", "Maze Rats pages", "Formulas"]
+        self.what_choice_var = tk.StringVar(value=self.what_choices)
 
-        self.what = tk.Listbox(self.what_frame, listvariable=what_choice_var, width=int(WIDTH/5), height=HEIGHT)
+        self.what = tk.Listbox(self.what_frame, listvariable=self.what_choice_var, width=int(WIDTH/5), height=HEIGHT)
         self.what.grid(column=0, row=0, sticky=(N, S, E, W))
-        self.what.bind("<<ListboxSelect>>", self.do_what)
+        self.what.bind("<<ListboxSelect>>", self._do_what)
 
-    def gen_page(self):
+    def _gen_page(self):
         "generates the page frame, for choosing, inspecting, or editing which 'page', formula or table"
         self.page_frame = ridge_frame(self.main)
         self.page_frame.grid(column=1, row=0, sticky=(N, S, E, W))
@@ -82,9 +82,9 @@ class ListMaster:  # pylint: disable=too-many-instance-attributes
 
         self.page = tk.Listbox(self.page_frame, listvariable=self.page_choices, width=int(3*WIDTH/5), height=HEIGHT)
         self.page.grid(column=0, row=0, sticky=(N, S, E, W))
-        self.page.bind("<<ListboxSelect>>", self.do_page)
+        self.page.bind("<<ListboxSelect>>", self._do_page)
 
-    def gen_results(self):
+    def _gen_results(self):
         "generates the result frame, for viewing the results, inspecting or editing formulas or tables."
         self.result_frame = ridge_frame(self.main)
         self.result_frame.grid(column=2, row=0, sticky=(N, S, E, W))
@@ -94,12 +94,12 @@ class ListMaster:  # pylint: disable=too-many-instance-attributes
         self.result = tk.Listbox(self.result_frame, listvariable=self.results, width=int(4*WIDTH/5), height=HEIGHT)
         self.result.grid(column=0, row=0, sticky=(N, S, E, W))
 
-    def gen_buttons(self):
+    def _gen_buttons(self):
         "generate button frame and some buttons; must be called before other gen_*_buttons. "
         self.button_frame = ttk.Frame(self.result_frame)
         self.button_frame.grid(column=0, row=1, sticky=(E, W))
 
-        self.reroll = ttk.Button(self.button_frame, text="Re-Roll", command=self.do_reroll)
+        self.reroll = ttk.Button(self.button_frame, text="Re-Roll", command=self._do_reroll)
         self.reroll.grid(column=0, row=0)
 
         self.num_pages_label = ttk.Label(self.button_frame, text="No. Pages")
@@ -111,23 +111,23 @@ class ListMaster:  # pylint: disable=too-many-instance-attributes
         self.num_pages = tk.Entry(self.button_frame, textvariable=self.num_pages_var)
         self.num_pages.grid(column=1, row=1)
 
-    def gen_form_buttons(self):
+    def _gen_form_buttons(self):
         "generates the extra buttons for dealing with formulas"
-        self.gen_xls = ttk.Button(self.button_frame, text="Generate .xls file", command=self.do_xls)
+        self.gen_xls = ttk.Button(self.button_frame, text="Generate .xls file", command=self._do_xls)
         self.gen_xls.grid(column=1, row=0)
 
-    def gen_clipboard(self):
+    def _gen_clipboard(self):
         "button to copy to clipboard."
-        self.clip_copy = ttk.Button(self.button_frame, text="Copy to clip board", command=self.copy_clip)
+        self.clip_copy = ttk.Button(self.button_frame, text="Copy to clip board", command=self._copy_clip)
         self.clip_copy.grid(column=2, row=0)
 
-    def copy_clip(self):
+    def _copy_clip(self):
         "copy the results to the clipboard"
         self.root.clipboard_clear()  # clear the clipboard because we are setting its contents
         # this is safe because the button that calls this method will not be shown unless results has contents
         self.root.clipboard_append("\n".join(self.result_list))
 
-    def config_grid(self):
+    def _config_grid(self):
         "calls column/rowconfigure on all our frames to set weights for resizing"
         self.root.rowconfigure(0, weight=1)
         self.root.columnconfigure(0, weight=1)
@@ -147,25 +147,25 @@ class ListMaster:  # pylint: disable=too-many-instance-attributes
         self.button_frame.columnconfigure(1, weight=1)
         self.button_frame.columnconfigure(2, weight=1)
 
-    def grid_form(self):
+    def _grid_form(self):
         "grids buttons for formulas."
         self.gen_xls.grid()
         self.num_pages_label.configure(text="No. Pages")
 
-    def ungrid_form(self):
+    def _ungrid_form(self):
         "ungrids buttons for formulas"
         self.gen_xls.grid_remove()
         self.num_pages_label.configure(text="No. to Roll")
 
-    def grid_roll(self):
+    def _grid_roll(self):
         "adds reroll and clip_copy to the button_frame"
         self.button_frame.grid()
 
-    def ungrid_roll(self):
+    def _ungrid_roll(self):
         "removes reroll and clip_copy from the button frame"
         self.button_frame.grid_remove()
-        
-    def do_what(self, e):  # pylint: disable=unused-argument
+
+    def _do_what(self, e):  # pylint: disable=unused-argument
         "handles the 'what' column, which is the top level category, and fills out the page choices"
         sel = self.what.curselection()
 
@@ -173,29 +173,29 @@ class ListMaster:  # pylint: disable=too-many-instance-attributes
             self.cur_page = self.what_list[sel[0]]
             self.page_choices.set([n for n, _ in self.cur_page])
             self.results.set([])
-            self.ungrid_roll()
-            self.ungrid_form()
+            self._ungrid_roll()
+            self._ungrid_form()
 
-    def do_page(self, e):  # pylint: disable=unused-argument
+    def _do_page(self, e):  # pylint: disable=unused-argument
         "handles the 'page' column to choose which table, page, or formula to generate from; triggers re-roll"
         sel = self.page.curselection()
         if len(sel) == 1:
             self.form_name, self.form = self.cur_page[sel[0]]
-            self.grid_roll()
+            self._grid_roll()
 
             if isinstance(self.form, dat.Formula):
-                self.grid_form()
+                self._grid_form()
             else:
-                self.ungrid_form()
+                self._ungrid_form()
 
-            self.do_reroll()
+            self._do_reroll()
 
-    def do_reroll(self):
+    def _do_reroll(self):
         "handles the re-roll button, generates and populates the results column"
         if self.form is not None:
             match(self.form):
                 case list():
-                    self.update_num()
+                    self._update_num()
                     self.result_list = [self.form_name] + [dat.gen_list(self.form) for _ in range(self.num)]
                 case dat.Formula():
                     self.result_list = [
@@ -210,14 +210,14 @@ class ListMaster:  # pylint: disable=too-many-instance-attributes
 
         self.results.set(self.result_list)
 
-    def update_num(self):
+    def _update_num(self):
         "updates self.num"
         num = int_nun(self.num_pages_var.get())  # try to get the number
         self.num = num if num else SINGLE  # if we don't have a number num is None
 
-    def do_xls(self):
+    def _do_xls(self):
         "Handles the Generate .xls button, currently makes double sided three hole punched 8 1/2 x 11 pages."
-        self.update_num()
+        self._update_num()
         dat.manufacture(self.form, self.path, self.form_name, self.num)
 
 
