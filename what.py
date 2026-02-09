e35deabc432bb75bcb7a5fcb8bcfb1c5ebd137aa
Mark <esoterik_@hotmail.com>
2026-02-09 00:45:59 -0800

    listpanel refactor

--- what.py
+++ what.py
@@ -2,30 +2,19 @@
 
 import tkinter as tk
 from tkinter import E, N, S, W, ttk
-from tkinter.simpledialog import askstring
 
-from enums import HEIGHT, WIDTH,  State
-
-# type alias
-table = list[str | list | tuple]  # pylint: disable=invalid-name
+from enums import State, table
+from gendata import Formula, MetaFormula
+from ListPanel import ListPanel
 
 
-class WhatPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-instance-attributes
+class WhatPanel(ListPanel):  # pylint: disable=too-many-ancestors,too-many-instance-attributes
     "generates the What panel, for choosing what top level category to use"
-    def __init__(self, parent, **kwargs):
-        super().__init__(parent, borderwidth=5, relief="ridge", **kwargs)
-        self.grid(column=0, row=0, sticky=(N, S, E, W))
-        self._parent = parent
-        self.last_selection = None
+    def __init__(self, parent: "MainPanel", **kwargs):
+        super().__init__(parent, 0, **kwargs)
+        self._parent: "MainPanel" = parent
 
         self.what_list: list[tuple[str, table]] = []
-        self.choices: list[str] = []
-        self.what_choice_var = tk.StringVar()
-
-        self.what = tk.Listbox(self, listvariable=self.what_choice_var, width=int(WIDTH/5), height=HEIGHT)
-        self.what.grid(column=0, row=0, sticky=(N, S, E, W))
-        self.what.bind("<<ListboxSelect>>", self.do_what)
-
         self.button_frame = ttk.Frame(self)
         self.button_frame.grid(column=0, row=1, sticky=(E, W))
 
@@ -60,8 +49,6 @@ class WhatPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-insta
         self.edit_button.grid(column=1, row=1, sticky=(N, S, E, W))
         self.edit_button.grid_remove()
 
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
 
         self.button_frame.rowconfigure(0, weight=1)
@@ -69,15 +56,22 @@ class WhatPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-insta
         self.button_frame.columnconfigure(0, weight=1)
         self.button_frame.columnconfigure(1, weight=2)
 
+    def accept_edit(self, newtext: str) -> bool:
+        "Must be overridden to edit"
+
+        if newtext == "":
+            del self.choices[self.last_selection]
+            del self.what_list[self.last_selection]
+        elif self._is_safe(newtext):
+            self.choices[self.last_selection] = newtext
+            _, tab = self.what_list[self.last_selection]
+            self.what_list[self.last_selection] = (newtext, tab)
+
     def set_what_data(self, lst: list[tuple[str, table]], choice: list[str]):
         "sets the values for the what panel, the list needs to have data in it, choice should have the labels."
         self.what_list = lst
         self.choices = choice
-        self.update_what()
-
-    def update_what(self):
-        "Updates the what_choice_var to self.choices"
-        self.what_choice_var.set(self.choices)
+        self._update_lbox()
 
     def set_mode(self):
         "Tells the parent to set the mode"
@@ -100,31 +94,46 @@ class WhatPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-insta
         "gets the top level what lists"
         return self.what_list, self.choices
 
-    def do_what(self, *args):  # pylint: disable=unused-argument
+    def do_lbox_sel(self, *args):  # pylint: disable=unused-argument
         "handles the 'what' column, which is the top level category, and fills out the page choices"
-        sel = self.what.curselection()
-
-        if len(sel) == 1:
-            self._parent.set_page(self.what_list[sel[0]])
+        if (sel := self._sel()) is not None:
+            self._parent.set_page(self.what_list[sel])
             self._parent.set_result([])
             self._parent.set_item(("", []))
-            self.last_selection = sel[0]
 
     def add_cat(self):
         "add a new category to the what panel"
-        name = askstring("Add Category", "Category Name")
-
-        if name:
-            self.choices.append(name)
-            self.what_list.append([])
-            self.update_what()
+        self.choices.append("")
+        self.what_list.append(("",[]))
+        self._update_lbox()
+        self.last_selection = len(self.choices)-1
+        self.lbox.activate(self.last_selection)
+        return self._start_edit("")
 
     def edit_cat(self):
         "edit the name of a category on the what panel"
         if self.last_selection:  # we don't want to to edit the special all tables entry at 0,
-            self.choices[self.last_selection] = askstring(
-                "Edit Category",
-                "New Category Name?",
-                initialvalue=self.choices[self.last_selection]
-            )
-            self.update_what()
+            self.lbox.activate(self.last_selection)
+            return self._start_edit(self.choices[self.last_selection])
+        return "return"
+
+    def add_to_all(self, tab: tuple[str, table]):
+        "add a new table to the all list"
+        # if not self._parent.has_name(tab[0]): # would be a double check our caller checks fairst
+        self.what_list[0][1].append(tab)
+
+    def rename(self, name: str, newname: str) -> bool:
+        "renames a table"
+        if self._parent.has_name(newname):
+            return False
+
+        for i, x in self.what_list[0]:
+            if x[0] == name:
+                match(x[1]):
+                    case MetaFormula() | Formula():
+                        x[1].name = newname
+                    case _:
+                        pass
+                self.what_list[0][i] = (newname, x[1])
+                return True
+
