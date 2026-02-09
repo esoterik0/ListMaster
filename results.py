cde10136bae89be714a381c9a7fb0404313e87b3
Mark <esoterik_@hotmail.com>
2026-02-09 00:45:29 -0800

    listpanel refactor

--- results.py
+++ results.py
@@ -1,21 +1,19 @@
 "the results panel: displays the results"
 
+import re
 import tkinter as tk
 from enum import Enum
 from tkinter import E, N, S, W, filedialog, ttk
 
 import gendata as dat
-from edinsitu import start_edit
-from enums import HEIGHT, SINGLE, WIDTH, State
-
-#type alias
-table = list[str | list | tuple]  # pylint: disable=invalid-name
+from enums import SINGLE, State, table
+from ListPanel import ListPanel
 
 LOG = "rolls.log"
 
 
 class Widgets(Enum):
-    "enums to define which widgets to grid and ungrid at various points"
+    "enums to define which widgets to grid and ungrid at any point in time"
     ROLL = "ROLL"
     FORM = "FORM"
 
@@ -29,7 +27,7 @@ def int_nun(s: str) -> int | None:
         return None
 
 
-class ResultsPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-instance-attributes
+class ResultsPanel(ListPanel):  # pylint: disable=too-many-ancestors,too-many-instance-attributes
     """
     generates the result panel frame
     displays results
@@ -42,79 +40,72 @@ class ResultsPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-in
         todo
     """
 
-    def __init__(self, parent, **kwargs):  # pylint: disable=too-many-statements
-        self._parent = parent
+    def __init__(self, parent: "MainPanel", **kwargs):  # pylint: disable=too-many-statements
+        # initialize ...
+        super().__init__(parent, 2, **kwargs)  #  .. our super class
+        self._parent: "MainPanel" = parent
+
         # state variables
         self.num = SINGLE
         self.path = ""
         self.item = None
         self.item_name = ""
-        self.last_selection = None
         self.logfile = LOG
+        self.sep_pat = re.compile(r"({[\w ]*)}")
 
-        # initialize ...
-        super().__init__(parent, borderwidth=5, relief="ridge", **kwargs)  #  .. our super class
-        self.grid(column=2, row=0, sticky=(N, S, E, W))  # ... oursevles   # type: ignore # this is the way tk works
+        # add double click.
+        self.lbox.bind("<Double-1>", self.do_double_select)
 
         # Create and grid our child members; some members will also be grid_removed()ed
 
-        # Results
-        self.result_list = []  # our list; self.results.set(self.result_list)
-        self.results = tk.StringVar()  # tk list
-        self.result = tk.Listbox(self, listvariable=self.results, width=int(4*WIDTH/5), height=HEIGHT)
-        self.result.grid(column=0, row=0, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
-        self.result.bind("<<ListboxSelect>>", self.do_select)
-        self.result.bind("<Double-1>", self.do_double_select)
-
         # holds the roll buttons, it can be swaped with a different frame
         self.roll_button_frame = ttk.Frame(self)
-        self.roll_button_frame.grid(column=0, row=1, sticky=(E, W)) # type: ignore # this is the way tk works
+        self.roll_button_frame.grid(column=0, row=1, sticky=(E, W))
         self.roll_button_frame.grid_remove()
 
         # buttons for roll mode
         self.roll_buttons = {}
         self.roll_buttons["reroll"] = ttk.Button(self.roll_button_frame, text="Re-Roll", command=self.do_reroll)
-        self.roll_buttons["reroll"].grid(column=0, row=0, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
+        self.roll_buttons["reroll"].grid(column=0, row=0, sticky=(N, S, E, W))
         self.roll_buttons["num_pages_label"] = ttk.Label(self.roll_button_frame, text="No. Pages")
-        self.roll_buttons["num_pages_label"].grid(column=0, row=1, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
+        self.roll_buttons["num_pages_label"].grid(column=0, row=1, sticky=(N, S, E, W))
         self.num_pages_var = tk.StringVar()
         self.num_pages_var.set(f"{SINGLE}")
         self.roll_buttons["num_pages"] = tk.Entry(self.roll_button_frame, textvariable=self.num_pages_var)
-        self.roll_buttons["num_pages"].grid(column=1, row=1, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
+        self.roll_buttons["num_pages"].grid(column=1, row=1, sticky=(N, S, E, W))
         self.roll_buttons["clip_copy"] = ttk.Button(
             self.roll_button_frame,
             text="Copy to clipboard",
             command=self._copy_clip
         )
-        self.roll_buttons["clip_copy"].grid(column=1, row=0, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
+        self.roll_buttons["clip_copy"].grid(column=1, row=0, sticky=(N, S, E, W))
         self.roll_buttons["gen_xls"] = ttk.Button(self.roll_button_frame, text="Generate .xls file", command=self.do_xls)
-        self.roll_buttons["gen_xls"].grid(column=2, row=1, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
+        self.roll_buttons["gen_xls"].grid(column=2, row=1, sticky=(N, S, E, W))
         self.roll_buttons["get_path"] = ttk.Button(self.roll_button_frame, text="Choose path", command=self.do_path)
-        self.roll_buttons["get_path"].grid(column=2, row=0, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
+        self.roll_buttons["get_path"].grid(column=2, row=0, sticky=(N, S, E, W))
         self.roll_buttons["set_log"] = ttk.Button(self.roll_button_frame, text="Set log file", command=self.do_set_log)
-        self.roll_buttons["set_log"].grid(column=0, row=2, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
+        self.roll_buttons["set_log"].grid(column=0, row=2, sticky=(N, S, E, W))
         self.roll_buttons["log_roll"] = ttk.Button(self.roll_button_frame, text="Reroll & log", command=self.do_logroll)
-        self.roll_buttons["log_roll"].grid(column=1, row=2, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
+        self.roll_buttons["log_roll"].grid(column=1, row=2, sticky=(N, S, E, W))
         self.roll_buttons["log"] = ttk.Button(self.roll_button_frame, text="Log the roll", command=self.do_log)
-        self.roll_buttons["log"].grid(column=2, row=2, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
+        self.roll_buttons["log"].grid(column=2, row=2, sticky=(N, S, E, W))
 
         # holds the edit buttons; swapped in during edit mode.
         self.edit_button_frame = ttk.Frame(self)
-        self.edit_button_frame.grid(column=0, row=1, sticky=(E, W)) # type: ignore # this is the way tk works
+        self.edit_button_frame.grid(column=0, row=1, sticky=(E, W))
         self.edit_button_frame.grid_remove()
 
         # buttons for edit mode
         self.edit_buttons = {}
         self.edit_buttons["add_item"] = ttk.Button(self.edit_button_frame, text="Add Item", command=self.do_add)
-        self.edit_buttons["add_item"].grid(column=0, row=0, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
+        self.edit_buttons["add_item"].grid(column=0, row=0, sticky=(N, S, E, W))
         self.edit_buttons["edit_item"] = ttk.Button(self.edit_button_frame, text="Edit Item", command=self.do_edit)
-        self.edit_buttons["edit_item"].grid(column=0, row=1, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
+        self.edit_buttons["edit_item"].grid(column=0, row=1, sticky=(N, S, E, W))
         self.edit_buttons["done"] = ttk.Button(self.edit_button_frame, text="Done Editing", command=self.do_done)
-        self.edit_buttons["done"].grid(column=1, row=1, sticky=(N, S, E, W)) # type: ignore # this is the way tk works
+        self.edit_buttons["done"].grid(column=1, row=1, sticky=(N, S, E, W))
 
         # configure the grids
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
         # 3x3 grid
         for i in range(3):
             self.roll_button_frame.rowconfigure(i, weight=1)
@@ -123,6 +114,41 @@ class ResultsPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-in
         for i in range(2):
             self.edit_button_frame.columnconfigure(i, weight=1)
             self.edit_button_frame.rowconfigure(i, weight=1)
+
+    def accept_edit(self, newtext: str) -> bool:
+        "Must be overridden to edit"
+        if self.last_selection is None:
+            return
+
+        if len(newtext) == 0:
+            return
+
+        if self._is_safe(newtext):
+            self.item[self.last_selection] = newtext
+        # the above if will take care of strings that don't have {} in them
+        elif newtext.count("{") == newtext.count("}"): # check that we have pairs of {}
+            out = [x for x in self.sep_pat.split(newtext) if x] # filter empty strings
+            #assert len(out) > 0
+            if len(out) == 1:
+                #assert out[0][0] == "{"
+                if self._is_safe(tab := out[0][1:]):
+                    self.item[self.last_selection] = self._parent.get_name_index(tab)[1]
+            else:
+                put = []
+                for item in out:
+                    if item[0]=="{":
+                        if self._is_safe(tab := item[1:]):
+                            put.append(self._parent.get_name_index(tab)[1])
+                    elif self._is_safe(item):
+                        put.append(item)
+                #assert len(put) > 0:
+                self.item[self.last_selection] = tuple(put)
+
+        self.set_result()
+        #check for
+        # normal text
+        # {table}
+        # ; for Formulas
 
     def set_item(self, form: tuple[str, dat.Formula] | tuple[str, table]):
         "dispatches based on mode"
@@ -147,18 +173,21 @@ class ResultsPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-in
     def set_item_edit(self):
         "sets the item to edit"
         if self.item:
-            self.result_list = []
+            self.choices = []
             match self.item:
                 case dat.MetaFormula():
-                    self.result_list = [",".join(
-                        self._get_name(itm) for itm in item.formula
+                    self.choices = [",".join(
+                        self.get_name(itm) for itm in item.formula
                         ) for item in self.item.formula
                     ]  # + ["label: " + b for b in self.item.labels]
-                    # todo figure out labels for MetaFormula; and how to make/edit them
+                    # todo figure out lables for MetaFormula; and how to make/edit them
                 case dat.Formula():
-                    self.result_list = [self._get_name(item) for item in self.item.formula]
+                    self.choices = [
+                        f"{label};{self.get_name(item)}"
+                        for item, label in zip(self.item.formula, self.item.formula.label)
+                    ]
                 case list():
-                    self.result_list = [self._get_name(item) for item in self.item]
+                    self.choices = [self.get_name(item) for item in self.item]
             self.set_result()
 
     def set_item_roll(self):
@@ -189,8 +218,8 @@ class ResultsPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-in
         "sets the results list, used for setting and clearing the list"
         # use results_list as is if called without a parameter
         if lst is not None:  # empty lists are allowed
-            self.result_list = lst
-        self.results.set(self.result_list)  # type: ignore # this is the way tk works
+            self.choices = lst
+        self._update_lbox()
 
     def ungrid_set(self):
         "removes widgets from the grid bassed on mode"
@@ -231,9 +260,9 @@ class ResultsPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-in
                 match(self.item):
                     case list():
                         self._update_num()
-                        self.result_list = [self.item_name] + [dat.gen_list(self.item) for _ in range(self.num)]
+                        self.choices = [self.item_name] + [dat.gen_list(self.item) for _ in range(self.num)]
                     case dat.MetaFormula() | dat.Formula():
-                        self.result_list = [f"{type(self.item).__name__}: {self.item_name}"] + [
+                        self.choices = [f"{type(self.item).__name__}: {self.item_name}"] + [
                             b + ": " + o  # label: object format for formulas
                             for b, o in zip(
                                 self.item.labels,
@@ -243,7 +272,7 @@ class ResultsPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-in
                     case _:
                         pass
             else:
-                self.result_list = []  # clear the list
+                self.choices = []  # clear the list
             self.set_result()
 
     def do_path(self):
@@ -267,7 +296,7 @@ class ResultsPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-in
     def do_add(self):
         "add item to item button"
 
-    def do_select(self, *args):  # pylint: disable=unused-argument
+    def do_lbox_sel(self, *args):  # pylint: disable=unused-argument
         "selection clicking"
         self._sel()
 
@@ -278,16 +307,12 @@ class ResultsPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-in
         if self._parent.state == State.EDIT:
             return self.do_edit()
 
-        return None
+        return "return"
 
     def do_edit(self):
         "popup an edit window in place"
-        return start_edit(
-            self.result,
-            self.last_selection,
-            self._check_accept_edit,
-            self.result_list[self.last_selection]
-        )
+        if self.last_selection is not None:
+            self._start_edit(self.choices[self.last_selection])
 
     def do_set_log(self):
         "set log file button"
@@ -302,18 +327,18 @@ class ResultsPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-in
     def do_log(self):
         "log the results button"
         with open(self.logfile, "a", encoding="utf-8") as f:
-            print(f"rolling {self.item_name}", *self.result_list, sep='\n', end='\n\n', file=f)
+            print(f"rolling {self.item_name}", *self.choices, sep='\n', end='\n\n', file=f)
 
     def _copy_clip(self):
         "copy the results to the clipboard"
-        self._parent.clipboard("\n".join(self.result_list))
+        self._parent.do_clipboard("\n".join(self.choices))
 
     def _update_num(self):
         "updates self.num"
         num = int_nun(self.num_pages_var.get())  # try to get the number
         self.num = num if num else SINGLE  # if we don't have a number num is None
 
-    def _get_name(self, item: dat.MetaFormula | dat.Formula | list | tuple | str) -> str:
+    def get_name(self, item: dat.MetaFormula | dat.Formula | list | tuple | str) -> str:
         """
         get a translated name from an item
 
@@ -326,9 +351,9 @@ class ResultsPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-in
             in the form of (name1name2...namen)
             strings can be concatenated together, so each tuple can not have more than one string in
             a row, a table must be between any strings in the tuple, one can have multiple tables, but
-            they should probably be separated by a space " ".
+            they should probably be separated by a space " " or something.
 
-            ex. "pre {table1} {table2} mid{table3} post"
+            ex. "pre{table1} {table2}mid{table3} post"
 
         strings are passed unchanged
         """
@@ -337,16 +362,6 @@ class ResultsPanel(ttk.Frame):  # pylint: disable=too-many-ancestors,too-many-in
                 _, name = self._parent.item_index(item)
                 return f"{{{name}}}"
             case tuple():
-                return f"({"".join(self._get_name(itm) for itm in item)})"
+                return f"({"".join(self.get_name(itm) for itm in item)})"
             case str():
                 return item
-
-    def _sel(self):
-        "current->last selection logic helper function"
-        sel = self.result.curselection()
-
-        if len(sel) == 1:
-            self.last_selection = sel[0]
-
-    def _check_accept_edit(self, val: str):
-        "check and accept an edit"
