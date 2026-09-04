ab8d09727b74078a50db984794b2a9149f37cc9a
Mark <esoterik_@hotmail.com>
2026-09-03 19:50:34 -0700

    enum

--- enums.py
+++ enums.py
@@ -14,6 +14,7 @@ SINGLE = 1
 TEXTSIZE = 12
 MENUSIZE = 12
 LOG = "rolls.log"
+FNAME = "tables.dat"
 
 # weights describe relative movement so bigger movement is shrinking and growing faster smaller
 COLWEIGHT = (1,1,1) # numbers stay larger at smaller sizes
